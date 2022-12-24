9c9329e98a7fc37975e6ca40c5ab18889ad23a63
Corso <58090876+yosbel-penate@users.noreply.github.com>
2022-12-23 23:48:26 -0500

    add models to admin.py

--- Models/admin.py
+++ Models/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models.Choice import Choice
+from .models.Dimension import Dimension
+from .models.Indicator import Indicator
+from .models.MeasurementCriterion import MeasurementCriterion
+from .models.Poll import Poll
+from .models.Question import Question
+from .models.Respondent import Respondent
+from .models.Scale import Scale
+from .models.ScaleLabel import ScaleLabel 
+from .models.Variable import Variable
 
-# Register your models here.
+admin.site.register(Choice)
+admin.site.register(Dimension)
+admin.site.register(Indicator)
+admin.site.register(MeasurementCriterion)
+admin.site.register(Poll)
+admin.site.register(Question)
+admin.site.register(Respondent)
+admin.site.register(Scale)
+admin.site.register(ScaleLabel)
+admin.site.register(Variable)
--- Models/models/Scale.py
+++ Models/models/Scale.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Scale(models.Model):
-    description = models.TextField(default='')
+    description = models.CharField(max_length=200)
     name = models.CharField(max_length=128, default='')
     max_value = models.IntegerField(default=0)
     interval = models.IntegerField(default=0)
