76ba819cda82926c982c1b02eff428b2a3f36072
EmanuelBorrajero <emanuelborrajero@gmail.com>
2022-12-25 00:04:42 -0500

    Cree App Poll y Agregue las Apps al archivo Setings

--- settings.py
+++ settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Models'
+    'Models',
+    'Metrics',
+    'Poll',
 ]
 
 MIDDLEWARE = [
