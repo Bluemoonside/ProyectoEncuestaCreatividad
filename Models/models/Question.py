c00c8012c95413072282ad0957eeb923c421eedd
EmanuelBorrajero <emanuelborrajero@gmail.com>
2022-12-30 18:09:29 -0500

    cambie los modelos ahora se ve mejor

--- Models/models/Question.py
+++ Models/models/Question.py
@@ -8,10 +8,11 @@ class Question(models.Model):
     ('M', 'Medium'),
     ('L', 'Large'),
     )
-    question_text = models.CharField(max_length=200)
+    tittle = models.CharField(max_length=50, default="Pregunta Sin Titulo")
+    question_text = models.TextField(max_length=200)
     pub_date = models.DateTimeField('date published', default=datetime.datetime.now())
     type = models.CharField(max_length=1, choices=TYPE_QUESTION, default='')
     measurement_criterion = models.ForeignKey(MeasurementCriterion, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
-        return f"Pregunta: {self.id}"
+        return f"Pregunta({self.id}): {self.tittle}"
