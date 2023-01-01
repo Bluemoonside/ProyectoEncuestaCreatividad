1038dd544af3f82d4546d096d0a91919aff18e5e
EmanuelBorrajero <emanuelborrajero@gmail.com>
2023-01-01 15:08:39 -0500

    arregle las urls y cree la base de la vista principal

--- Poll/urls.py
+++ Poll/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from Poll.views import *
 
 urlpatterns = [
-    path('polllist/', PollList.as_view(), name='polllist'),
-    path('questionlist/', QuestionList.as_view(), name='questionlist'),
-    path('createpoll/', PollCreate.as_view(), name='createpoll'),
-    path('createquestion/', QuestionCreate.as_view(), name='createquestion'),
-    path('polllist/updatepoll/<pk>', PollUpdate.as_view(), name='updatepoll'),
-    path('questionlist/updatequestion/<pk>', QuestionUpdate.as_view(), name='updatequestion'),
-    path('polllist/deletepoll/<pk>', PollDelete.as_view(), name='deletepoll'),
-    path('questionlist/deletequestion/<pk>', QuestionDelete.as_view(), name='deletequestion'),
+    path('list/', PollList.as_view(), name='polllist'),
+    path('question/list/', QuestionList.as_view(), name='questionlist'),
+    path('create/poll/', PollCreate.as_view(), name='createpoll'),
+    path('create/question/', QuestionCreate.as_view(), name='createquestion'),
+    path('list/update/poll/<pk>', PollUpdate.as_view(), name='updatepoll'),
+    path('list/update/question/<pk>', QuestionUpdate.as_view(), name='updatequestion'),
+    path('list/delete/poll/<pk>', PollDelete.as_view(), name='deletepoll'),
+    path('list/delete/question/<pk>', QuestionDelete.as_view(), name='deletequestion'),
 ]    
--- ProyectoEncuestaCreatividad/settings.py
+++ ProyectoEncuestaCreatividad/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ProyectoEncuestaCreatividad.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ProyectoEncuestaCreatividad/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
