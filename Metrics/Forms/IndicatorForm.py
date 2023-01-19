599730bc67aa024a6a70d1e4aab17c51ce96d11c
Roniel Navarro <117552739+Roniel1510@users.noreply.github.com>
2023-01-19 15:40:40 +0100

    haciendo validaciones

--- Metrics/Forms/IndicatorForm.py
+++ Metrics/Forms/IndicatorForm.py
@@ -17,3 +17,14 @@ class IndicatorForm(forms.ModelForm):
             'weigh': forms.NumberInput(attrs={'class':'form-control'}),
             'dimensions':forms.SelectMultiple(attrs={'class':'form-control'}),
         }
+    def clean_description(self):
+        description=self.cleaned_data['description']
+        for c in description:
+            if c in'`!@#$%^&*()_=+-}{][></"\|0123456789~':
+              raise forms.ValidationError('Solo se aceptan letras en este campo.') 
+        return description
+    def clean_weigh(self):
+        weigh=self.cleaned_data['weigh']
+        if weigh <1:
+            raise forms.ValidationError('El peso debe ser mayor que 0.') 
+        return weigh      
--- Poll/Forms/QuestionForm.py
+++ Poll/Forms/QuestionForm.py
@@ -22,3 +22,15 @@ class QuestionForm(forms.ModelForm):
             'type': forms.Select(),
             'measurement_criterion': forms.Select(attrs={'class': 'form-control'}),
         }
+    def clean_tittle(self):
+        tittle=self.cleaned_data['tittle']
+        for c in tittle:
+            if c in'`!@#$%^&*()_=+-}{][><\/"1234567890|~':
+              raise forms.ValidationError('Solo se aceptan letras en este campo.') 
+        return tittle 
+    def clean_question_text(self):
+        question_text=self.cleaned_data['question_text']
+        for c in question_text:
+            if c in'`!@#$%^&*()_=+-}{][></"\|1234567890~':
+              raise forms.ValidationError('Solo se aceptan letras en este campo.') 
+        return question_text 
